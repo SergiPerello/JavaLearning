16b614cf7de8f610e4f0bf886c94cb353377167f
Sergi Perello <sergi.perello@mataro.epiaedu.cat>
2019-05-21 18:51:26 +0200

    progress

--- uf3_Projectes/a99_jocPenjat.java
+++ uf3_Projectes/a99_jocPenjat.java
@@ -1,11 +1,12 @@
 package uf3_Projectes;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class a99_jocPenjat {
 	
-	String word = "guacamole";
+	String word = "llapis";
 	
 	public static void main(String[] args) {
 		a99_jocPenjat program = new a99_jocPenjat();
@@ -13,11 +14,17 @@ public class a99_jocPenjat {
 	}
 	
 	private void controller() {
+		ArrayList<Integer> positions = new ArrayList<Integer>();
 		int wordLength = word.length();
-		char[] baseWord = word.toCharArray();
 		char[] finalWord = drawUnderscores(wordLength);
 		char userChar = askCase();
-		checkCase(wordLength, baseWord, finalWord ,userChar);
+		positions = checkChar(userChar, wordLength);
+		
+		if (positions.size()!=0) {
+			finalWord = replaceChar(positions, finalWord, userChar);
+			System.out.println(finalWord);
+		}
+		else System.out.println("Error, el char no esta al string");
 	}
 	
 	private char[] drawUnderscores(int wordLength) {
@@ -41,11 +48,28 @@ public class a99_jocPenjat {
 		return userChar;
 	}
 	
-	private void checkCase(int wordLength, char[] baseWord, char[] finalWord, char userChar) {
-        Boolean[] matches = new Boolean[wordLength];
-        for (int i = 0; i < wordLength; i++)
-            matches[i] = userChar == baseWord[i];
+	private ArrayList<Integer> checkChar(char userChar, int wordLength) {
+        ArrayList<Integer> positions = new ArrayList<Integer>();
+        int currentPosition = 0;
+        while (currentPosition != -1 && currentPosition < wordLength) {
+        	currentPosition = word.indexOf(userChar, currentPosition);
+        	if (currentPosition != -1) {
+        		positions.add(currentPosition);
+        		currentPosition++;
+        	}
+        }
+        return positions;
     }
+	
+	private char[] replaceChar(ArrayList<Integer> positions, char[] finalWord, char userChar) {
+		int currentPosition;
+		int positionsSize = positions.size();
+		for (int i=0;i<positionsSize;i++) {
+			currentPosition = positions.get(i);
+			finalWord[currentPosition] = userChar;
+		}
+		return finalWord;
+	}
 }
 /*
 Dibuja una línea en blanco para cada letra de la palabra
